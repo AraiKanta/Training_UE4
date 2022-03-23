6001a4107194a36380bf3fc6e0aeaa631cee44aa
K_Arai00 <attckonk@gmail.com>
2022-03-23 20:21:59 +0900

    カウントダウンチュートリアルに使用するクラスの追加

--- Source/MyTraining_Project/CountDown.cpp
+++ Source/MyTraining_Project/CountDown.cpp
@@ -1,27 +1,61 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "CountDown.h"
+#include "Countdown.h"
+#include "Components/TextRenderComponent.h"
 
 // Sets default values
-ACountDown::ACountDown()
+ACountdown::ACountdown()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	CountdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountdownNumber"));
+	CountdownText->SetHorizontalAlignment(EHTA_Center);
+	CountdownText->SetWorldSize(150.0f);
+	RootComponent = CountdownText;
+    CountdownTime = 3;
 }
 
 // Called when the game starts or when spawned
-void ACountDown::BeginPlay()
+void ACountdown::BeginPlay()
 {
 	Super::BeginPlay();
 	
+    //新しく更新した関数の呼び出しを追加して ACountdown::BeginPlay のテキスト表示を初期化します。
+    //そして 1 秒に一回カウントダウンを進め、更新を行うタイマーを設定します。
+    UpdateTimerDisplay();
+    GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ACountdown::AdvanceTimer, 1.0f, true);
 }
 
 // Called every frame
-void ACountDown::Tick(float DeltaTime)
+void ACountdown::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
+//ACountdown::UpdateTimerDisplay はテキスト表示を更新して、残り時間、または時間切れの場合はゼロを表示します。
+//ゲームに ACountdown を初めてスポーンした時にこのコードを実行して、 CountdownTime 変数がゼロになるまで 1 秒に一回コードを実行します。
+void ACountdown::UpdateTimerDisplay()
+{
+	CountdownText->SetText(FString::FromInt(FMath::Max(CountdownTime, 0)));
+}
+
+void ACountdown::AdvanceTimer()
+{
+    --CountdownTime;
+    UpdateTimerDisplay();
+    if (CountdownTime < 1)
+    {
+        //We're done counting down, so stop running the timer.
+        GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
+        CountdownHasFinished();
+    }
+}
+
+void ACountdown::CountdownHasFinished()
+{
+    //Change to a special readout
+    CountdownText->SetText(TEXT("GO!"));
+}
--- Source/MyTraining_Project/CountDown.h
+++ Source/MyTraining_Project/CountDown.h
@@ -4,16 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "CountDown.generated.h"
+#include "Components/TextRenderComponent.h"
+#include "Countdown.generated.h"
 
 UCLASS()
-class MYTRAINING_PROJECT_API ACountDown : public AActor
+class MYTRAINING_PROJECT_API ACountdown : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ACountDown();
+	ACountdown();
 
 protected:
 	// Called when the game starts or when spawned
@@ -23,4 +24,15 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	int32 CountdownTime;
+	UTextRenderComponent* CountdownText;
+	void UpdateTimerDisplay();
+
+	//関数の実行に Timer を割り当てると、必ず Timer Handle が与えられます。
+	//カウントダウン終了時に Timer をシャットダウンできるようにこのハンドルをしっかりと維持します。
+	//時間をカウントダウンする関数と、この関数をコントロールするために必要な Timer Handle を Countdown.h のクラス定義に追加しましょう。
+	//この作業を行うついでに、カウントダウン終了時に何か特別なことを行う関数も追加しましょう。
+	void AdvanceTimer();
+	void CountdownHasFinished();
+	FTimerHandle CountdownTimerHandle;
 };
